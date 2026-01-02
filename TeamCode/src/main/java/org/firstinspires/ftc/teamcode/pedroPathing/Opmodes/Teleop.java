afa5f398636cb1e3cfd3e89951ee8557cbf9eb44
JohnConway21 <150561068+JohnConway21@users.noreply.github.com>
2026-01-02 11:23:14 -0500

    auto intake + drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Opmodes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Opmodes/Teleop.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode.pedroPathing.Opmodes;
+
 import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
@@ -10,99 +11,121 @@ import com.pedropathing.paths.Path;
 import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants.Constants;
+import org.firstinspires.ftc.teamcode.pedroPathing.Vision.GreenArtifactPipeline;
 
 import java.util.function.Supplier;
 
 @Configurable
 @TeleOp
 public class Teleop extends OpMode {
+
+    // Follower stuff
     private Follower follower;
-    public static Pose startingPose; //See ExampleAuto to understand how to use this
+    public static Pose startingPose;
     private boolean automatedDrive;
     private Supplier<PathChain> pathChain;
     private TelemetryManager telemetryM;
     private boolean slowMode = false;
     private double slowModeMultiplier = 0.5;
 
+    // Vision & intake stuff
+    private VisionPortal visionPortal;
+    private GreenArtifactPipeline pipeline;
+    private DcMotor intake;
+
     @Override
     public void init() {
+        // --- Follower initialization ---
         follower = Constants.createFollower(hardwareMap);
         follower.setStartingPose(startingPose == null ? new Pose() : startingPose);
         follower.update();
+
         telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
 
-        pathChain = () -> follower.pathBuilder() //Lazy Curve Generation
+        pathChain = () -> follower.pathBuilder()
                 .addPath(new Path(new BezierLine(follower::getPose, new Pose(45, 98))))
                 .setHeadingInterpolation(HeadingInterpolator.linearFromPoint(follower::getHeading, Math.toRadians(45), 0.8))
                 .build();
+
+        // --- Vision & intake initialization ---
+        pipeline = new GreenArtifactPipeline();
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        intake.setPower(0);
+
+        visionPortal = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .addProcessor(pipeline)
+                .build();
+
+        telemetry.addLine("TeleOp & Vision Initialized");
+        telemetry.update();
     }
 
     @Override
     public void start() {
-        //The parameter controls whether the Follower should use break mode on the motors (using it is recommended).
-        //In order to use float mode, add .useBrakeModeInTeleOp(true); to your Drivetrain Constants in Constant.java (for Mecanum)
-        //If you don't pass anything in, it uses the default (false)
         follower.startTeleopDrive();
     }
 
     @Override
     public void loop() {
-        //Call this once per loop
+        // --- Follower teleop logic ---
         follower.update();
         telemetryM.update();
 
         if (!automatedDrive) {
-            //Make the last parameter false for field-centric
-            //In case the drivers want to use a "slowMode" you can scale the vectors
+            double lx = -gamepad1.left_stick_y;
+            double ly = -gamepad1.left_stick_x;
+            double rx = -gamepad1.right_stick_x;
 
-            //This is the normal version to use in the TeleOp
-            if (!slowMode) follower.setTeleOpDrive(
-                    -gamepad1.left_stick_y,
-                    -gamepad1.left_stick_x,
-                    -gamepad1.right_stick_x,
-                    false // Robot Centric
-            );
+            if (slowMode) {
+                lx *= slowModeMultiplier;
+                ly *= slowModeMultiplier;
+                rx *= slowModeMultiplier;
+            }
 
-                //This is how it looks with slowMode on
-            else follower.setTeleOpDrive(
-                    -gamepad1.left_stick_y * slowModeMultiplier,
-                    -gamepad1.left_stick_x * slowModeMultiplier,
-                    -gamepad1.right_stick_x * slowModeMultiplier,
-                    false // Robot Centric
-            );
+            follower.setTeleOpDrive(lx, ly, rx, false); // Robot Centric
         }
 
-        //Automated PathFollowing
+        // Automated path following
         if (gamepad1.aWasPressed()) {
             follower.followPath(pathChain.get());
             automatedDrive = true;
         }
 
-        //Stop automated following if the follower is done
         if (automatedDrive && (gamepad1.bWasPressed() || !follower.isBusy())) {
             follower.startTeleopDrive();
             automatedDrive = false;
         }
 
-        //Slow Mode
-        if (gamepad1.rightBumperWasPressed()) {
-            slowMode = !slowMode;
+        // Slow mode toggles
+        if (gamepad1.rightBumperWasPressed()) slowMode = !slowMode;
+        if (gamepad1.xWasPressed()) slowModeMultiplier += 0.25;
+        if (gamepad2.yWasPressed()) slowModeMultiplier -= 0.25;
+
+        // --- Vision-based intake logic ---
+        if (pipeline.greenDetected) {
+            intake.setPower(-1); // run intake if green detected
+        } else {
+            intake.setPower(0);
         }
 
-        //Optional way to change slow mode strength
-        if (gamepad1.xWasPressed()) {
-            slowModeMultiplier += 0.25;
-        }
-
-        //Optional way to change slow mode strength
-        if (gamepad2.yWasPressed()) {
-            slowModeMultiplier -= 0.25;
-        }
-
+        // --- Telemetry ---
         telemetryM.debug("position", follower.getPose());
         telemetryM.debug("velocity", follower.getVelocity());
         telemetryM.debug("automatedDrive", automatedDrive);
+        telemetry.addData("Green Detected", pipeline.greenDetected);
+        telemetry.addData("Largest Area", pipeline.largestArea);
+        telemetry.update();
+    }
+
+    @Override
+    public void stop() {
+        visionPortal.close();
+        intake.setPower(0);
     }
 }
