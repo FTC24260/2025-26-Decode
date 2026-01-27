e8eb92e71a6af6f9ffb917e36506f5e5cf2965c4
JohnConway21 <150561068+JohnConway21@users.noreply.github.com>
2026-01-27 16:07:21 -0500

    Drive 12

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Opmodes/Arti9BlueFront.java
@@ -0,0 +1,239 @@
+package org.firstinspires.ftc.teamcode.pedroPathing.Opmodes;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants.Constants;
+
+@Autonomous(name = "Arti9 Blue Front Auto CHASSIS ONLY", group = "Autonomous")
+public class Arti9BlueFront extends OpMode {
+
+    private Follower follower;
+    private Timer pathTimer;
+    private int pathState;
+
+    private final Pose startPose = new Pose(22, 127, Math.toRadians(145));
+    private final Pose shootPreloadPose = new Pose(60, 84, Math.toRadians(180));
+
+    private final Pose pickup11Pose = new Pose(38, 84, Math.toRadians(180));
+    private final Pose pickup12Pose = new Pose(33, 84, Math.toRadians(180));
+    private final Pose pickup13Pose = new Pose(28, 84, Math.toRadians(180));
+    private final Pose shoot6Pose = new Pose(60, 84, Math.toRadians(200));
+
+    private final Pose pickup21Pose = new Pose(38, 60, Math.toRadians(180));
+    private final Pose pickup22Pose = new Pose(33, 60, Math.toRadians(180));
+    private final Pose pickup23Pose = new Pose(28, 60, Math.toRadians(180));
+    private final Pose shoot9Pose = new Pose(60, 84, Math.toRadians(200));
+
+    private final Pose pickup31Pose = new Pose(38, 36, Math.toRadians(180));
+    private final Pose pickup32Pose = new Pose(33, 36, Math.toRadians(180));
+    private final Pose pickup33Pose = new Pose(28, 36, Math.toRadians(180));
+    private final Pose shoot12Pose = new Pose(60, 84, Math.toRadians(200));
+
+    private PathChain shootPreload, pickup11, pickup12, pickup13, shoot6,
+            pickup21, pickup22, pickup23, shoot9,
+            pickup31, pickup32, pickup33, shoot12;
+
+    public void buildPaths() {
+
+        shootPreload = follower.pathBuilder()
+                .addPath(new BezierLine(startPose, shootPreloadPose))
+                .setLinearHeadingInterpolation(startPose.getHeading(), shootPreloadPose.getHeading())
+                .build();
+
+        pickup11 = follower.pathBuilder()
+                .addPath(new BezierLine(shootPreloadPose, pickup11Pose))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        pickup12 = follower.pathBuilder()
+                .addPath(new BezierLine(pickup11Pose, pickup12Pose))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        pickup13 = follower.pathBuilder()
+                .addPath(new BezierLine(pickup12Pose, pickup13Pose))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        shoot6 = follower.pathBuilder()
+                .addPath(new BezierLine(pickup13Pose, shoot6Pose))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        pickup21 = follower.pathBuilder()
+                .addPath(new BezierLine(shoot6Pose, pickup21Pose))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        pickup22 = follower.pathBuilder()
+                .addPath(new BezierLine(pickup21Pose, pickup22Pose))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        pickup23 = follower.pathBuilder()
+                .addPath(new BezierLine(pickup22Pose, pickup23Pose))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        shoot9 = follower.pathBuilder()
+                .addPath(new BezierLine(pickup23Pose, shoot9Pose))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        pickup31 = follower.pathBuilder()
+                .addPath(new BezierLine(shoot9Pose, pickup31Pose))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        pickup32 = follower.pathBuilder()
+                .addPath(new BezierLine(pickup31Pose, pickup32Pose))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        pickup33 = follower.pathBuilder()
+                .addPath(new BezierLine(pickup32Pose, pickup33Pose))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        shoot12 = follower.pathBuilder()
+                .addPath(new BezierLine(pickup33Pose, shoot12Pose))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+    }
+
+    public void autonomousPathUpdate() {
+        switch (pathState) {
+
+            case 0:
+                if (!follower.isBusy()) {
+                    follower.followPath(shootPreload, true);
+                    setPathState(1);
+                }
+                break;
+
+            case 1:
+                if (!follower.isBusy()) {
+                    follower.followPath(pickup11, true);
+                    setPathState(2);
+                }
+                break;
+
+            case 2:
+                if (!follower.isBusy()) {
+                    follower.followPath(pickup12, true);
+                    setPathState(3);
+                }
+                break;
+
+            case 3:
+                if (!follower.isBusy()) {
+                    follower.followPath(pickup13, true);
+                    setPathState(4);
+                }
+                break;
+
+            case 4:
+                if (!follower.isBusy()) {
+                    follower.followPath(shoot6, true);
+                    setPathState(5);
+                }
+                break;
+
+            case 5:
+                if (!follower.isBusy()) {
+                    follower.followPath(pickup21, true);
+                    setPathState(6);
+                }
+                break;
+
+            case 6:
+                if (!follower.isBusy()) {
+                    follower.followPath(pickup22, true);
+                    setPathState(7);
+                }
+                break;
+
+            case 7:
+                if (!follower.isBusy()) {
+                    follower.followPath(pickup23, true);
+                    setPathState(8);
+                }
+                break;
+
+            case 8:
+                if (!follower.isBusy()) {
+                    follower.followPath(shoot9, true);
+                    setPathState(9);
+                }
+                break;
+
+            case 9:
+                if (!follower.isBusy()) {
+                    follower.followPath(pickup31, true);
+                    setPathState(10);
+                }
+                break;
+
+            case 10:
+                if (!follower.isBusy()) {
+                    follower.followPath(pickup32, true);
+                    setPathState(11);
+                }
+                break;
+
+            case 11:
+                if (!follower.isBusy()) {
+                    follower.followPath(pickup33, true);
+                    setPathState(12);
+                }
+                break;
+
+            case 12:
+                if (!follower.isBusy()) {
+                    follower.followPath(shoot12, true);
+                    setPathState(-1);
+                }
+                break;
+
+        }
+    }
+
+    public void setPathState(int state) {
+        pathState = state;
+        pathTimer.resetTimer();
+    }
+
+    @Override
+    public void init() {
+        pathTimer = new Timer();
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(startPose);
+        buildPaths();
+    }
+
+    @Override
+    public void start() {
+        pathTimer.resetTimer();
+        setPathState(0);
+    }
+
+    @Override
+    public void loop() {
+        follower.update();
+        autonomousPathUpdate();
+
+        Pose p = follower.getPose();
+        telemetry.addData("Path State", pathState);
+        telemetry.addData("X", p.getX());
+        telemetry.addData("Y", p.getY());
+        telemetry.addData("Heading", Math.toDegrees(p.getHeading()));
+        telemetry.update();
+    }
+}
