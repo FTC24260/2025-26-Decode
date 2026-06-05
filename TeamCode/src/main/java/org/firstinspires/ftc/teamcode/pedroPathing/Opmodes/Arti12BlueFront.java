0f2223f5cd792afa961f27019653d2f67fe7879a
Haridevsum <haridevsumesh@gmail.com>
2026-06-05 19:24:11 -0400

    Updated Spindex Pos for Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Opmodes/Arti12BlueFront.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Opmodes/Arti12BlueFront.java
@@ -22,8 +22,8 @@ public class Arti12BlueFront extends OpMode {
     private Servo leftIndex, rightIndex, flicker;
     private DcMotor turret;
 
-    private final double[] shootPositions = {0.29, 0.378, 0.472};
-    private final double[] intakePositions = {0.144, 0.236, 0.33};
+    private final double[] shootPositions = {0.1756, 0.2956, 0.4216};
+    private final double[] intakePositions = {0.2311, 0.3589, 0.4756};
     private final double flickerUp = 0.575;
     private final double flickerDown = 0.795;
 
